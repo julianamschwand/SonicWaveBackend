6fca34bf58a2ca72571adf6ec86acba7fe9f4f70
julianamschwand <j.amschwand@hotmail.com>
2025-07-31 03:36:36 +0200

    Changed sql datatypes of ids to uint

--- db/db-setup.sql
+++ db/db-setup.sql
@@ -1,69 +1,69 @@
 create table UserData (
-  userDataId int primary key auto_increment,
+  userDataId int unsigned primary key auto_increment,
   username varchar(50) unique,
   email varchar(254) unique,
   passwordHash varchar(255),
   userRole enum('user','admin','owner'),
   approved boolean,
-  queueIndex int
+  queueIndex int unsigned
 );
 
 create table Playlists (
-  playlistId int primary key auto_increment,
+  playlistId int unsigned primary key auto_increment,
   playlistName varchar(30),
   playlistDescription text,
   playlistCoverFileName char(36),
-  fk_UserDataId int,
+  fk_UserDataId int unsigned,
   foreign key (fk_UserDataId) references UserData(userDataId) on delete cascade
 );
 
 create table Artists (
-  artistId int primary key auto_increment,
+  artistId int unsigned primary key auto_increment,
   artistName varchar(100) unique,
   artistDescription text
 );
 
 create table Songs (
-  songId int primary key auto_increment,
+  songId int unsigned primary key auto_increment,
   songFileName char(36) unique,
   title varchar(100),
   genre varchar(20),
   duration int,
-  releaseYear int,
+  releaseYear year,
   isFavorite boolean default false,
   lastPlayed timestamp,
-  fk_UserDataId int,
+  fk_UserDataId int unsigned,
   foreign key (fk_UserDataId) references UserData(userDataId) on delete cascade
 );
 
 create table SongArtists(
-  songArtistId int primary key auto_increment,
-  fk_SongId int,
-  fk_ArtistId int,
+  songArtistId int unsigned primary key auto_increment,
+  fk_SongId int unsigned,
+  fk_ArtistId int unsigned,
   foreign key (fk_SongId) references Songs(songId) on delete cascade,
   foreign key (fk_ArtistId) references Artists(artistId) on delete cascade
 );
 
 create table PlaylistSongs (
-  playlistSongId int primary key auto_increment,
-  fk_PlaylistId int,
-  fk_SongId int,
+  playlistSongId int unsigned primary key auto_increment,
+  fk_PlaylistId int unsigned,
+  fk_SongId int unsigned,
   foreign key (fk_PlaylistId) references Playlists(playlistId) on delete cascade,
   foreign key (fk_SongId) references Songs(songId) on delete cascade
 );
 
 create table OneTimePasswords (
-  otpId int primary key auto_increment,
+  otpId int unsigned primary key auto_increment,
   otp char(6),
-  attemptsRemaining int default 3,
-  fk_UserDataId int,
+  attemptsRemaining tinyint default 3,
+  fk_UserDataId int unsigned,
   foreign key (fk_UserDataId) references UserData(userDataId) on delete cascade
 );
 
 create table QueuedSongs (
-  queuedSongId int primary key auto_increment,
-  fk_UserDataId int,
-  fk_SongId int,
+  queuedSongId int unsigned primary key auto_increment,
+  fk_UserDataId int unsigned,
+  fk_SongId int unsigned,
   foreign key (fk_UserDataId) references UserData(userDataId) on delete cascade,
   foreign key (fk_SongId) references Songs(songId) on delete cascade
 );

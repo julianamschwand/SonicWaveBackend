824a3cb8855ab0b8130d8bcec8df056c3494ef2b
julianamschwand <j.amschwand@hotmail.com>
2025-10-18 22:41:13 +0200

    Fixed the newgrounds refresh function

--- cron/jobs/refreshNewgrounds.js
+++ cron/jobs/refreshNewgrounds.js
@@ -5,8 +5,14 @@ export default async function refreshNewgrounds() {
     const browser = await puppeteer.launch({ headless: true })
     const page = await browser.newPage()
     await page.goto("https://www.newgrounds.com", { waitUntil: "networkidle2" })
-    setTimeout(() => browser.close(), 2000)
+    await page.waitForFunction(
+      (keyword) => !document.documentElement.innerHTML.includes(keyword),
+      { timeout: 120000 },
+      "NG Guard"
+    )
+    
     console.log("Successfully refreshed newgrounds")
+    await browser.close()
   } catch (error) {
     console.error("Error while refreshing newgrounds", error)
   }
